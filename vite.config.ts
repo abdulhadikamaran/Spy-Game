812ed132ed74be4fe9e8f4fbd0b3916cba14b456
Abdulhadi Kamaran <abdulhadikamaran2005@gmail.com>
2026-03-31 23:04:35 +0300

    Fix target browser compatibility for older phones causing black screens

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   build: {
-    target: 'esnext',
+    target: 'es2020',
     minify: 'esbuild',
     cssMinify: true,
     sourcemap: false,
